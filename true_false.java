b7e4a68cdcbb5516536134436b17f825a63cf426
VarshiniManokaran <varshini6678@gmail.com>
2024-08-05 19:39:57 +0530

    Add files via upload

---
+++ true_false.java
@@ -0,0 +1,34 @@
+public class true_false {
+    
+        public static void main(String[] args) {
+            
+            
+            
+        
+    
+    
+    boolean isRainCome = false;
+    if( isRainCome){
+        System.out.println("u take an umrella☔");
+    }
+    else{
+        System.out.println("Enjoy the sunshine🌞");
+    }
+
+
+    boolean homework=true;
+        
+    
+
+    if(homework){
+        System.out.println("good job!👍");
+
+    }
+    else{
+        System.out.println("You Try to finished ur home work😐");
+    }
+}
+}
+    
+    
+
